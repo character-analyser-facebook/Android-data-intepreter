4b98896ae85e6587e11b999beeef58bc84f56724
N.Sanjeev kumar <sanjeevitcit@gmail.com>
2018-02-28 23:00:43 +0530

    Initial commit
    Includes gradle and manifest configurations for facebook SDK
    
    Signed-off-by: N.Sanjeev kumar <sanjeevitcit@gmail.com>

---
+++ app/src/main/java/com/example/sanjeev/intelligentorganizer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sanjeev.intelligentorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
